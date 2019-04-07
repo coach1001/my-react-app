72cfd5231122a1e061715955c2b6ac97e7820541
Francois Weber <coach1001@gmail.com>
2019-04-07 22:52:52 +0200

    add build to git

---
+++ build/config.js
@@ -0,0 +1,7 @@
+window.configGA = {
+	// API_DB : "http://localhost/restapi",
+	// API_AD : "http://localhost/adservice",
+	API_DB : "http://ec2-3-17-207-57.us-east-2.compute.amazonaws.com/restapi",
+	API_AD : "http://ec2-3-17-207-57.us-east-2.compute.amazonaws.com/adservice",
+	asyncTimeout : 2000
+}
