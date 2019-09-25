3e881f03e377150f69d7b3b844b5a6ffb69b1ebc
Francois Weber <coach1001@gmail.com>
2019-09-25 20:56:08 +0200

    Config file

--- build/config.js
+++ build/config.js
@@ -1,7 +1,17 @@
 window.configGA = {
+	
+	// DOCKER LOCAL
+	// API_DB : "http://localhost/restapi",
+	// API_AD : "http://localhost/adservice",
+
+	// DOCKER REMOTE
+	// API_DB : "http://ec2-3-17-207-57.us-east-2.compute.amazonaws.com/restapi",
+	// API_AD : "http://ec2-3-17-207-57.us-east-2.compute.amazonaws.com/adservice",
+	
+	// DEVELOPMENT
 	API_DB : "http://localhost:3003",
-	//"API_AD":"http://localhost/adservice"
-	//"API_DB":"http://localhost:3003",
+	// API_AD : "http://localhost:3004",
 	API_AD : "http://localhost/adservice",
-	asyncTimeout : 2000
+	
+	asyncTimeout : 30000
 }
